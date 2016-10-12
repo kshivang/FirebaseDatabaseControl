7b4ab15bbd521bd2d59e83749aa06c15e897c087
kshivang <shivang.iitk@gmail.com>
2016-10-13 00:41:46 +0530

    Initial commit

---
+++ app/src/main/java/com/shivang/firebasedatabasecontrol/SplashScreen.java
@@ -0,0 +1,18 @@
+package com.shivang.firebasedatabasecontrol;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by kshivang on 13/10/16.
+ * Simple Splash Screen
+ */
+
+public class SplashScreen extends AppCompatActivity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+    }
+}
