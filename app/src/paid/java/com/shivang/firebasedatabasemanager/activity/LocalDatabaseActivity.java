643c45f69db46d1ccb077820873b5353acb822b9
kshivang <shivang.iitk@gmail.com>
2017-03-15 02:52:25 +0530

    Multiple Variants added

---
+++ app/src/paid/java/com/shivang/firebasedatabasemanager/activity/LocalDatabaseActivity.java
@@ -0,0 +1,149 @@
+/*
+ *   Copyright (C) 2017  Shivang<shivang.iitk@gmail.com>
+ *
+ *   This file is part of Firebase Database Manager.
+ *
+ *       Firebase Database Manager is free software: you can redistribute it and/or modify
+ *       it under the terms of the GNU General Public License as published by
+ *       the Free Software Foundation, either version 3 of the License, or
+ *       (at your option) any later version.
+ *
+ *       Firebase Database Manager is distributed in the hope that it will be useful,
+ *       but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *       MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *       GNU General Public License for more details.
+ *
+ *       You should have received a copy of the GNU General Public License
+ *       along with Firebase Database Manager.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package com.shivang.firebasedatabasemanager.activity;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import com.shivang.firebasedatabasemanager.R;
+import com.shivang.firebasedatabasemanager.fragment.LocalDatabaseFragment;
+import com.shivang.firebasedatabasemanager.misc.JsonCreator;
+
+import java.util.ArrayList;
+
+import static com.shivang.firebasedatabasemanager.R.string.no_node_here;
+
+/**
+ * Created by kshivang on 22/10/16.
+ * Contributors may mention there name below
+ * Shivang
+ *
+ *
+ * This activity just hold the fragment
+ */
+
+//todo : Some fields to be updated
+public class LocalDatabaseActivity extends AppCompatActivity{
+//    private String currentUri = "";
+//    private String baseUri = "";
+
+    private View progressBarHolder;
+    private ProgressBar progressBar;
+    private TextView errorTest;
+    private View view;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.actvity_database);
+
+        findViewById(R.id.toolbar_view).setVisibility(View.GONE);
+        setSupportActionBar((Toolbar)findViewById(R.id.toolbar));
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setTitle("Local database");
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
+        progressBarHolder = findViewById(R.id.progress_bar_holder);
+        progressBar = (ProgressBar)  findViewById(R.id.progress_bar);
+        errorTest = (TextView) findViewById(R.id.error_text);
+        view = findViewById(R.id.fragment);
+        onLocalDatabaseFragment(JsonCreator.fileNames(this), null);
+    }
+
+    public void home(View view) {
+        finish();
+    }
+
+    private void onLocalDatabaseFragment(ArrayList<String> files, String response) {
+        if (files != null && files.size() > 0) {
+            getSupportFragmentManager().
+                    beginTransaction().replace(R.id.fragment,
+                    LocalDatabaseFragment.newInstance(files)).
+                    commit();
+        } else if (response != null) {
+            getSupportFragmentManager().
+                    beginTransaction().replace(R.id.fragment,
+                    LocalDatabaseFragment.newInstance(response)).
+                    commit();
+        } else {
+            progressBar.setVisibility(View.GONE);
+            view.setVisibility(View.GONE);
+            progressBarHolder.setVisibility(View.VISIBLE);
+            errorTest.setText(no_node_here);
+            errorTest.setVisibility(View.VISIBLE);
+        }
+    }
+
+    public void onFragmentSet(boolean isEmpty) {
+        progressBar.setVisibility(View.GONE);
+        if (isEmpty) {
+            progressBarHolder.setVisibility(View.VISIBLE);
+            errorTest.setText(no_node_here);
+            errorTest.setVisibility(View.VISIBLE);
+            view.setVisibility(View.GONE);
+        } else {
+            progressBarHolder.setVisibility(View.GONE);
+            errorTest.setVisibility(View.GONE);
+            view.setVisibility(View.VISIBLE);
+        }
+    }
+
+//    public void onForwardTransverse(String key) {
+//    }
+
+//    public void onRefresh() {
+//    }
+
+    private void onBackwardTransverse() {
+    }
+
+//    private void onUrlRequest(String url) {
+//
+//    }
+
+    @Override
+    public void onBackPressed() {
+//        if () {
+//            finish();
+//        } else {
+            onBackwardTransverse();
+//        }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                onBackPressed();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+}
---
+++ app/src/paid/java/com/shivang/firebasedatabasemanager/activity/SplashScreen.java
@@ -0,0 +1,100 @@
+/*
+ *   Copyright (C) 2017  Shivang<shivang.iitk@gmail.com>
+ *
+ *   This file is part of Firebase Database Manager.
+ *
+ *       Firebase Database Manager is free software: you can redistribute it and/or modify
+ *       it under the terms of the GNU General Public License as published by
+ *       the Free Software Foundation, either version 3 of the License, or
+ *       (at your option) any later version.
+ *
+ *       Firebase Database Manager is distributed in the hope that it will be useful,
+ *       but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *       MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *       GNU General Public License for more details.
+ *
+ *       You should have received a copy of the GNU General Public License
+ *       along with Firebase Database Manager.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package com.shivang.firebasedatabasemanager.activity;
+
+import android.animation.Animator;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.animation.AccelerateDecelerateInterpolator;
+
+import com.shivang.firebasedatabasemanager.R;
+import com.shivang.firebasedatabasemanager.revealAnimation.ViewAnimationUtils;
+
+
+/**
+ * Created by kshivang on 13/10/16.
+ * Simple Splash Screen
+ */
+
+public class SplashScreen extends AppCompatActivity{
+
+    View rlHolder;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        rlHolder = findViewById(R.id.rl_intro_holder);
+
+        rlHolder.post(new Runnable() {
+            @Override
+            public void run() {
+                int cx = (rlHolder.getLeft() + rlHolder.getRight()) / 2;
+                int cy = (rlHolder.getTop() + rlHolder.getBottom()) / 2;
+
+                // get the final radius for the clipping circle
+                int dx = Math.max(cx, rlHolder.getWidth() - cx);
+                int dy = Math.max(cy, rlHolder.getHeight() - cy);
+                float finalRadius = (float) Math.hypot(dx, dy);
+
+                // Android native animator
+
+                Animator animator =
+                        ViewAnimationUtils.createCircularReveal(rlHolder, 0,
+                                cy, 0, finalRadius * 2);
+                animator.setInterpolator(new AccelerateDecelerateInterpolator());
+                animator.setDuration(1000);
+                animator.start();
+                animator.addListener(new Animator.AnimatorListener() {
+                    @Override
+                    public void onAnimationStart(Animator animation) {
+
+                    }
+
+                    @Override
+                    public void onAnimationEnd(Animator animation) {
+                        new Handler().postDelayed(new Runnable() {
+                            @Override
+                            public void run() {
+                                startActivity(new Intent(SplashScreen.this, LandingActivity.class));
+                                finish();
+                            }
+                        }, 2000);
+                    }
+
+                    @Override
+                    public void onAnimationCancel(Animator animation) {
+
+                    }
+
+                    @Override
+                    public void onAnimationRepeat(Animator animation) {
+
+                    }
+                });
+            }
+        });
+    }
+}
